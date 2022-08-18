284816607c7969477fa5a0fd3f30dbaf6d676f7f
kim-seonkyu <seonkyu93@gmail.com>
2022-08-18 23:12:36 +0900

    update css

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ let randomNum = 0;
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
+let resultAreaImg = document.querySelector(".main-img");
 let resetButton = document.getElementById("reset-button");
 let chanceArea = document.getElementById("chance-area");
 let chances = 5;
 let gameOver = false;
 let history = [];
 
+console.log(resultAreaImg);
+
+chanceArea.textContent = `남은 기회 : ${chances}`;
 playButton.addEventListener("click", play);
 resetButton.addEventListener("click", reset);
 userInput.addEventListener("focus", function () {
@@ -35,20 +39,27 @@ function play() {
 
   chances--;
   chanceArea.textContent = `남은 기회 : ${chances}번`;
+  history.push(userValue);
 
   if (userValue < randomNum) {
+    resultAreaImg.src =
+      "https://c.tenor.com/A-ozELwp694AAAAC/thumbs-thumbs-up-kid.gif";
     resultArea.textContent = "UP!!";
   } else if (userValue > randomNum) {
+    resultAreaImg.src =
+      "https://c.tenor.com/5VOxlkxi63gAAAAC/thumbsdown-ryanreynolds.gif";
     resultArea.textContent = "DOWN!!";
   } else if (userValue == randomNum) {
+    resultAreaImg.src =
+      "https://c.tenor.com/NVlG12ERFzwAAAAM/clapping-leonardo-dicaprio.gif";
     resultArea.textContent = "정답!!!";
     gameOver = true;
   }
 
-  history.push(userValue);
-
   if (chances < 1) {
     gameOver = true;
+    resultAreaImg.src =
+      "https://media3.giphy.com/media/l4pLY0zySvluEvr0c/giphy.gif";
     resultArea.textContent = "실패 ㅠㅠ";
   }
 
@@ -58,9 +69,9 @@ function play() {
 }
 
 function reset() {
-  userInput.value = "";
-
   pickRandomNum();
+  userInput.value = "";
+  resultAreaImg.src = "https://t1.daumcdn.net/cfile/blog/99BF9D345ABA8C5811";
 
   resultArea.textContent = "이번엔 꼭 맞추세요!";
   gameOver = false;
